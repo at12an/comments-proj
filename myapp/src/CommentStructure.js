fb8e58ffa78a5d02f2580a6685b316d1b5135bc0
at12an <ahttran19@gmail.com>
2023-09-03 22:41:25 +1000

    code cleanup

--- myapp/src/CommentStructure.js
+++ myapp/src/CommentStructure.js
@@ -52,9 +52,14 @@ const CommentStructure = () => {
 
     return (
     <div>
+         {comments.map((c, index) => (
+            <div class='flex justify-center items-center'>
+                <p class="text-center box-content bg-gray-200 inline-block px-2 py-1 rounded-full absolute animate-jiggle" key={index} style={{color:c.color, top:c.y+"%",left:c.x+"%"}}>{c.comment}</p>
+            </div>
+        ))}
         <form onSubmit={handleSubmit} id="commentform" className='m-0 p-0'>
             <div class='flex justify-center items-center rounded-lg min-h-screen'>
-                <div class='flex justify-between  w-1/3 bg-gray-200 rounded-full hover:bg-gray-300'>
+                <div class='flex justify-between  w-1/3 bg-gray-200 rounded-full hover:bg-gray-300 focus:bg-gray-300'>
                     <div class="rounded-full w-12 h-12 flex justify-center items-center hover:scale-105">
                         <div class="rounded-full w-8 h-8" style={{backgroundColor:color}}>
                             <input 
@@ -70,7 +75,7 @@ const CommentStructure = () => {
                         value={comment} 
                         placeholder="Write an interesting message!" 
                         onChange={e => handleCommentChange(e)}
-                        class="flex-grow bg-transparent rounded-3xl h-12 px-4 text-gray-800 focus:outline-none  focus:text-gray-900"
+                        class="flex-grow bg-transparent rounded-3xl h-12 px-4 text-gray-800 focus:outline-none  focus:text-gray-900 "
                     ></input>
                     <SubmitButton />
                 </div>
@@ -79,13 +84,6 @@ const CommentStructure = () => {
             <br></br>
 
         </form>
-
-        {comments.map((c, index) => (
-            <div class='flex justify-center items-center'>
-                <p class="text-center box-content bg-gray-200 inline-block px-2 py-1 rounded-full absolute animate-jiggle" key={index} style={{color:c.color, top:c.y+"%",left:c.x+"%"}}>{c.comment}</p>
-            </div>
-        ))}
-
     </div>
     );
 };
