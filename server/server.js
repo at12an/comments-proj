e59ffe5fd905b25bfa8fbf1c354a8bbcb0408f4a
at12an <ahttran19@gmail.com>
2023-09-05 17:32:00 +1000

    readded server

--- server/server.js
+++ server/server.js
@@ -13,12 +13,6 @@ app.listen(port, () => {
     console.log(port);
 })
 
-
-app.get('/', (req, res) => {
-    console.log("Hi")
-});
-
-
 app.post('/post-comment', (req, res) => {
     const comment = req.body.comment;
     const color = req.body.color;
